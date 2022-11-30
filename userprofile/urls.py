83dff3a9aa3ea58dcc5b5e758a22577f1f07c3ca
Ellie <yokomone@yahoo.com>
2022-11-30 02:06:47 +0200

    Create urls.py

---
+++ userprofile/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from userprofile import views
+
+urlpatterns = [
+    path('signup', views.usersignup, name="signup"),
+    path('login', views.userlogin, name="login"),
+    path('logout', views.userlogout, name="logout"),
+    path('edit-profile', views.useredit, name="edit profile"),
+    path('delete', views.userdelete, name="delete profile"),
+    path('<uname>', views.show_profile, name="show profile"),
+]
