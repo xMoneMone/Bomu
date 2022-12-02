48f847904754be07abc634b57bb3ab16f9a9f53f
Ellie <yokomone@yahoo.com>
2022-12-03 00:00:56 +0200

    model for post gets created when post button is pressed

--- canvas/views.py
+++ canvas/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from posts.forms import CanvasPostForm
+from posts.models import CanvasPost
 
 
 @login_required(login_url='login')
 def canvas(request):
+    if request.method == 'POST':
+        canvas_post = CanvasPost(user=request.user, drawing=r'static/images/no-drawing.png')
+        canvas_post.save()
+        return redirect('post-canvas')
     return render(request, template_name="canvas-create.html")
 
 
