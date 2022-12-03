3acf4464b1173a26e467fd7fdcc7344c58b57b98
Ellie <yokomone@yahoo.com>
2022-12-03 06:20:59 +0200

    Update views.py

--- canvas/views.py
+++ canvas/views.py
@@ -1,29 +1,24 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from posts.forms import CanvasPostForm
-from posts.models import CanvasPost
+from django.contrib.auth.models import User
 
 
 @login_required(login_url='login')
 def canvas(request):
-    if request.method == 'POST':
-        canvas_post = CanvasPost(user=request.user, drawing=r'art/download_4.png')
-        canvas_post.save()
-        return redirect('post-canvas')
     return render(request, template_name="canvas-create.html")
 
 
 @login_required(login_url='login')
 def post_canvas(request):
-    canvas_post = CanvasPost.objects.filter(user=request.user).last()
-    if request.method == 'GET':
-        form = CanvasPostForm(instance=canvas_post, initial=canvas_post.__dict__)
-    else:
-        form = CanvasPostForm(request.POST or None, request.FILES or None, instance=canvas_post)
+    form = CanvasPostForm()
+    if request.method == 'POST':
+        form = CanvasPostForm(request.POST, request.FILES or None)
         if form.is_valid():
-            form.save()
+            posted_drawing = form.save(commit=False)
+            posted_drawing.user = request.user
+            posted_drawing.save()
             return redirect('home')
 
-    context = {"form": form,
-               "canvas_post": canvas_post}
+    context = {"form": form}
     return render(request, template_name="post-canvas.html", context=context)
