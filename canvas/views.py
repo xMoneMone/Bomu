9fedae532c3a6b43fc54bdeb8cbca56d9a0e29a6
Ellie <yokomone@yahoo.com>
2022-12-03 04:10:33 +0200

    added canvas posting

--- canvas/views.py
+++ canvas/views.py
@@ -7,7 +7,7 @@ from posts.models import CanvasPost
 @login_required(login_url='login')
 def canvas(request):
     if request.method == 'POST':
-        canvas_post = CanvasPost(user=request.user, drawing=r'static/images/no-drawing.png')
+        canvas_post = CanvasPost(user=request.user, drawing=r'art/download_4.png')
         canvas_post.save()
         return redirect('post-canvas')
     return render(request, template_name="canvas-create.html")
@@ -15,5 +15,15 @@ def canvas(request):
 
 @login_required(login_url='login')
 def post_canvas(request):
-    print(request.GET.get('canvas'))
-    return render(request, template_name="post-canvas.html")
+    canvas_post = CanvasPost.objects.filter(user=request.user).last()
+    if request.method == 'GET':
+        form = CanvasPostForm(instance=canvas_post, initial=canvas_post.__dict__)
+    else:
+        form = CanvasPostForm(request.POST or None, request.FILES or None, instance=canvas_post)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {"form": form,
+               "canvas_post": canvas_post}
+    return render(request, template_name="post-canvas.html", context=context)
