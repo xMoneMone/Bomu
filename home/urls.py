440d1aac71545759a8ec04b51a4c4a55410c0de5
Ellie <yokomone@yahoo.com>
2022-12-04 05:33:32 +0200

    added sorting to home page

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('new', views.home_new, name="home-new")
 ]
 
--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from posts.models import CanvasPost
+from django.db.models import Count
 
 
 def home(request):
+    posts = CanvasPost.objects.filter().annotate(num=Count('canvaslike')).order_by('-num')
+    context = {'posts': posts}
+    return render(request, template_name="home.html", context=context)
+
+
+def home_new(request):
     posts = CanvasPost.objects.all().order_by('-id')
     context = {'posts': posts}
     return render(request, template_name="home.html", context=context)
