c58863fa2b02bbd7999a8c95c20e7fcbb0fc62be
Ellie <yokomone@yahoo.com>
2022-12-05 01:06:50 +0200

    updated postdetails view to accommodate commenting

--- posts/views.py
+++ posts/views.py
@@ -1,23 +1,39 @@
 from django.shortcuts import render, redirect
 from posts.models import CanvasPost, CanvasLike, CanvasComment
-from posts.forms import CanvasPostForm
+from posts.forms import CanvasPostForm, CanvasCommentForm
 from django.contrib.auth.decorators import login_required
 from django.views import View
 
 
 class PostDetails(View):
     def get(self, request, pk):
-        form = CanvasComment()
+        form = CanvasCommentForm()
         cur_post = CanvasPost.objects.get(id=pk)
+        comments = CanvasComment.objects.filter(to_post=cur_post).order_by('-id')
         context = {"post": cur_post,
-                   "form": form}
+                   "form": form,
+                   "comments": comments}
         return render(request, 'post_details.html', context=context)
+
+    def post(self, request, pk):
+        form = CanvasCommentForm(request.POST)
+        cur_post = CanvasPost.objects.get(id=pk)
+        comments = CanvasComment.objects.filter(to_post=cur_post).order_by('-id')
+        if form.is_valid():
+            text = form.cleaned_data.get('comment')
+            comment = CanvasComment(user=request.user, to_post=cur_post, comment=text)
+            comment.save()
+            return redirect(request.META['HTTP_REFERER'])
+        else:
+            context = {"post": cur_post,
+                       "form": form,
+                       "comments": comments}
+            return render(request, 'post_details.html', context=context)
 
 
 @login_required(login_url='login')
 def post_edit(request, pk):
     cur_post = CanvasPost.objects.get(id=pk)
-
     if cur_post.user != request.user:
         return redirect('home')
 
