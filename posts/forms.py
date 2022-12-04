31bc075a8216218aa231f94f545f5ae25c9c4b4b
Ellie <yokomone@yahoo.com>
2022-12-04 20:19:50 +0200

    added comment form

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from posts.models import CanvasPost
+from posts.models import CanvasPost, CanvasComment
 
 
 class CanvasPostForm(forms.ModelForm):
     class Meta:
         model = CanvasPost
         fields = ['drawing', 'description']
+
+
+class CanvasCommentForm(forms.ModelForm):
+    class Meta:
+        model = CanvasComment
+        fields = ['comment']
