173d9c14d85b0b02b5bde6bb860ec05c6ddad40f
Ellie <yokomone@yahoo.com>
2022-12-04 05:46:02 +0200

    changed sorting buttons styling

--- home/views.py
+++ home/views.py
@@ -4,12 +4,16 @@ from django.db.models import Count
 
 
 def home(request):
+    tag = "popular"
     posts = CanvasPost.objects.filter().annotate(num=Count('canvaslike')).order_by('-num')
-    context = {'posts': posts}
+    context = {'posts': posts,
+               'tag': tag}
     return render(request, template_name="home.html", context=context)
 
 
 def home_new(request):
+    tag = "new"
     posts = CanvasPost.objects.all().order_by('-id')
-    context = {'posts': posts}
+    context = {'posts': posts,
+               'tag': tag}
     return render(request, template_name="home.html", context=context)
