698961121eab32a2b1f16c2837c8dbb4b4af31ab
Ellie <yokomone@yahoo.com>
2022-12-04 15:16:37 +0200

    changed home views from function-based to class-based

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('new', views.home_new, name="home-new")
+    path('', views.Home.as_view(), name="home"),
+    path('new', views.HomeNew.as_view(), name="home-new")
 ]
 
--- home/views.py
+++ home/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from posts.models import CanvasPost
 from django.db.models import Count
+from django.views import View
 
 
-def home(request):
-    tag = "popular"
-    posts = CanvasPost.objects.filter().annotate(num=Count('canvaslike')).order_by('-num')
-    context = {'posts': posts,
-               'tag': tag}
-    return render(request, template_name="home.html", context=context)
+class Home(View):
+
+    def get(self, request):
+        tag = "popular"
+        posts = CanvasPost.objects.filter().annotate(num=Count('canvaslike')).order_by('-num')
+        context = {'posts': posts,
+                   'tag': tag}
+        return render(request, template_name="home.html", context=context)
 
 
-def home_new(request):
-    tag = "new"
-    posts = CanvasPost.objects.all().order_by('-id')
-    context = {'posts': posts,
-               'tag': tag}
-    return render(request, template_name="home.html", context=context)
+class HomeNew(View):
+
+    def get(self, request):
+        tag = "new"
+        posts = CanvasPost.objects.all().order_by('-id')
+        context = {'posts': posts,
+                   'tag': tag}
+        return render(request, template_name="home.html", context=context)
