377e861e5f0c5dfe4bfbab2f84914c5ed7625ddc
Ellie <yokomone@yahoo.com>
2022-12-11 07:24:40 +0200

    customised admin displays

--- posts/admin.py
+++ posts/admin.py
@@ -2,15 +2,63 @@ from django.contrib import admin
 from posts.models import CanvasComment, CanvasPost, CanvasLike, CanvasSuperlike
 
 
+def shorten(string, val: int):
+    if not string:
+        return None
+    if len(string) > val:
+        return string[:val] + "..."
+    else:
+        return string
+
+
+@admin.register(CanvasComment)
 class CanvasCommentAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "to_post", "comment")
+    list_display = ("id", "author", "post", "comment_pv")
+
+    def comment_pv(self, obj):
+        comment = obj.comment
+        return shorten(comment, 20)
+
+    def post(self, obj):
+        post = obj.to_post
+        artist = str(post.user)
+        return f"post #{post.id} by {shorten(artist, 10)}"
+
+    def author(self, obj):
+        author = str(obj.user)
+        return shorten(author, 10)
 
 
+@admin.register(CanvasPost)
 class CanvasPostAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "description")
+    list_display = ("id", "artist", "description_pv", "likes")
+
+    def artist(self, obj):
+        return shorten(str(obj.user), 10)
+
+    def likes(self, obj):
+        result = CanvasLike.objects.filter(to_post=obj)
+        return len(result)
+
+    def description_pv(self, obj):
+        return shorten(obj.description, 20)
 
 
-admin.site.register(CanvasComment, CanvasCommentAdmin)
-admin.site.register(CanvasPost, CanvasPostAdmin)
-admin.site.register(CanvasLike)
-admin.site.register(CanvasSuperlike)
+@admin.register(CanvasLike)
+class CanvasLikeAdmin(admin.ModelAdmin):
+    list_display = ("id", "post")
+
+    def post(self, obj):
+        post = obj.to_post
+        artist = str(post.user)
+        return f"post #{post.id} by {shorten(artist, 10)}"
+
+
+@admin.register(CanvasSuperlike)
+class CanvasSuperlikeAdmin(admin.ModelAdmin):
+    list_display = ("id", "post")
+
+    def post(self, obj):
+        post = obj.to_post
+        artist = str(post.user)
+        return f"post #{post.id} by {shorten(artist, 10)}"
--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from userprofile.models import UserProfile
+from posts.admin import shorten
 
-admin.site.register(UserProfile)
+
+@admin.register(UserProfile)
+class USerProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "nick", "bio_pv")
+
+    def username(self, obj):
+        username = str(obj.user)
+        return shorten(username, 10)
+
+    def nick(self, obj):
+        return shorten(obj.nickname, 12)
+
+    def bio_pv(self, obj):
+        return shorten(obj.bio, 20)
