b17ad5a6990a198cf073aaf6755563c3cf17d4f7
Ellie <yokomone@yahoo.com>
2022-12-11 16:40:42 +0200

    type hinted pk in urls

--- posts/urls.py
+++ posts/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from posts import views
 
 urlpatterns = [
-    path('<pk>', views.PostDetails.as_view(), name="post-details"),
-    path('<pk>/edit', views.post_edit, name="post-edit"),
-    path('<pk>/delete', views.post_delete, name="post-delete"),
-    path('<pk>/like', views.post_like, name="post-like"),
-    path('<pk>/superlike', views.post_superlike, name="post-superlike"),
-    path('comment/<pk>/delete', views.comment_delete, name="comment-delete"),
-    path('comment/<pk>/edit', views.comment_edit, name="comment-edit")
+    path('<int:pk>', views.PostDetails.as_view(), name="post-details"),
+    path('<int:pk>/edit', views.post_edit, name="post-edit"),
+    path('<int:pk>/delete', views.post_delete, name="post-delete"),
+    path('<int:pk>/like', views.post_like, name="post-like"),
+    path('<int:pk>/superlike', views.post_superlike, name="post-superlike"),
+    path('comment/<int:pk>/delete', views.comment_delete, name="comment-delete"),
+    path('comment/<int:pk>/edit', views.comment_edit, name="comment-edit")
 ]
