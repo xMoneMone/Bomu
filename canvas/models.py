fafddab049c24074dd47e41113b19d33c843c0c8
Ellie <yokomone@yahoo.com>
2022-12-13 20:10:36 +0200

    added a palette model

--- canvas/models.py
+++ canvas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from canvas.validators import hex_code_validator
 
-# Create your models here.
+
+class Palette(models.Model):
+    title = models.CharField(max_length=20, unique=True)
+    c1 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c2 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c3 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c4 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c5 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c6 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c7 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c8 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c9 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c10 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c11 = models.CharField(max_length=7, validators=(hex_code_validator,))
+    c12 = models.CharField(max_length=7, validators=(hex_code_validator,))
