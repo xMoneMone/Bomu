961939962f17cbb88acc8f4946811d58eea3995f
Ellie <yokomone@yahoo.com>
2022-12-14 01:12:48 +0200

    fixed a naming inconsistency

--- canvas/urls.py
+++ canvas/urls.py
@@ -3,7 +3,7 @@ from canvas import views
 
 urlpatterns = [
     path('create', views.canvas, name="canvas"),
-    path('post', views.post_canvas, name="post-canvas"),
+    path('post', views.post_canvas, name="post canvas"),
     path('palettes', views.palettes, name="palettes"),
     path('palettes/add', views.palette_add, name="add palette"),
     path('palette/edit/<int:pk>', views.palette_edit, name="edit palette"),
