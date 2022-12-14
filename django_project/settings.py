e6f079504d101ca3c25739b89fbeeb96e904b614
Ellie <yokomone@yahoo.com>
2022-12-14 18:08:24 +0200

    aint working

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yw@h)xi6+9h$^jkzn-wa3xaxdbrx(mbbnv_4d*(#i9xr=2mpc-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG'))
+print(DEBUG)
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,12 +82,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cwfdgfbp',
-        'USER': 'cwfdgfbp',
-        'PASSWORD': 'yatNIj-3rMYBz_chXrykS5XvZZRVkJoK',
-        'HOST': 'surus.db.elephantsql.com',
-        'PORT': '5432',
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         'TEST': {
             'NAME': 'bomu_test'
         }
@@ -97,7 +99,6 @@ DATABASES = {
         # 'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
